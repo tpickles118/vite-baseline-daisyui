a6ef295fa6a33be72e4b07ca2537e53711d42927
tpickles118 <46704998+tpickles118@users.noreply.github.com>
2025-11-09 11:27:24 +0100

    initial commit, tailwindcss, daisy

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button class="btn btn-sm btn-primary" @click="count++">
+      count is {{ count }}
+    </button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <div class="badge badge-accent">Badge for DaisyUI</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
