73c20c93ebfaf1da3c75efd3eb1c1d45278c0fc2
tpickles118 <46704998+tpickles118@users.noreply.github.com>
2025-11-09 16:04:11 +0100

    updated readme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
         <h1 class="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
         <ul class="list-disc list-inside">
             <li><router-link to="/about">About Page</router-link></li>
-            <li><router-link to="/users-api">API Demo</router-link></li>
+            <li><router-link to="/users-api">Axios API Demo</router-link></li>
         </ul>
     </div>
 </template>
